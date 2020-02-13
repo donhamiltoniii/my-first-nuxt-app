498c5e214a7acd585c6aebe57f20b93e8b3ea478
Don Hamilton III <donhamiltoniii@gmail.com>
2020-02-12 21:11:53 -0500

    Add basic routing and linking

---
+++ pages/users/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>All users!</h1>
+    <div>
+      <input type="text" v-model="id" />
+      <button @click="onLoadUser">Smack that button</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: ""
+    };
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push(`/users/${this.id}`);
+    }
+  }
+};
+</script>
